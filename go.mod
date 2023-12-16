b575d6cc7223f9ffdf78f3d3f0a50188dddaf0af
Elliott Rhys <rhyseli97@gmail.com>
2023-12-16 15:04:15 +0000

    use dotenv package to keep db creds safe

--- go.mod
+++ go.mod
@@ -5,4 +5,5 @@ go 1.21.5
 require (
 	github.com/go-sql-driver/mysql v1.7.1 // indirect
 	github.com/gorilla/mux v1.8.1 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
 )
--- main.go
+++ main.go
@@ -4,24 +4,67 @@ import (
 	"database/sql"
 	"fmt"
 	"net/http"
+	"os"
 
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/gorilla/mux"
+	"github.com/joho/godotenv"
 )
 
-func main() {
-	db, err := sql.Open("mysql", "elliott@(127.0.0.1:3306)/gowebexamples")
-	if err != nil {
-		panic(err)
-	}
-	if err := db.Ping(); err != nil {
-		panic(err)
-	}
+var db *sql.DB
 
+func main() {
+	setup()
+
+	// Register application routes
 	r := mux.NewRouter()
+	r.HandleFunc("/database/books", createBooksTable).Methods("GET")
 	r.HandleFunc("/books/{author}/{title}", showBook).Methods("GET")
 
 	http.ListenAndServe(":8008", r)
+}
+
+func setup() {
+	// Dotenv initialization
+	if err := godotenv.Load(); err != nil {
+		panic(err)
+	}
+
+	// Database initialization
+	sqlConnectString := fmt.Sprintf(
+		"%s:%s@(%s:%s)/%s",
+		os.Getenv("DB_USER"),
+		os.Getenv("DB_PASSWORD"),
+		os.Getenv("DB_HOST"),
+		os.Getenv("DB_PORT"),
+		os.Getenv("DB_NAME"),
+	)
+	sqlDb, sqlErr := sql.Open("mysql", sqlConnectString)
+	if sqlErr != nil {
+		panic(sqlErr)
+	}
+	if err := sqlDb.Ping(); err != nil {
+		panic(err)
+	}
+	db = sqlDb
+}
+
+func createBooksTable(w http.ResponseWriter, r *http.Request) {
+	query := `
+		CREATE TABLE books (
+			id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+			author VARCHAR(255) NOT NULL,
+			title VARCHAR(255) NOT NULL,
+			created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+		);
+	`
+	_, err := db.Exec(query)
+	if err != nil {
+		fmt.Fprintln(w, err)
+	} else {
+		fmt.Fprintln(w, "Books table created")
+	}
+
 }
 
 func showBook(w http.ResponseWriter, r *http.Request) {
