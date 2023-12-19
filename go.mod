c889d8c161805486d357fddeb5189631df4d5470
Elliott Rhys <rhyseli97@gmail.com>
2023-12-19 23:14:13 +0000

    user registration

--- go.mod
+++ go.mod
@@ -13,4 +13,5 @@ require (
 	github.com/hashicorp/errwrap v1.1.0 // indirect
 	github.com/hashicorp/go-multierror v1.1.1 // indirect
 	go.uber.org/atomic v1.7.0 // indirect
+	golang.org/x/crypto v0.17.0 // indirect
 )
--- handlers/auth_handler.go
+++ handlers/auth_handler.go
@@ -1,10 +1,12 @@
 package handlers
 
 import (
+	"fmt"
 	"html/template"
 	"net/http"
 
 	"github.com/twoteesgh/go-library/services"
+	"golang.org/x/crypto/bcrypt"
 )
 
 type AuthHandler struct {
@@ -15,6 +17,7 @@ type User struct {
 	id         int
 	name       string
 	email      string
+	password   string
 	created_at string
 	updated_at string
 }
@@ -37,7 +40,22 @@ func (h *AuthHandler) ShowRegisterPage(w http.ResponseWriter, r *http.Request) {
 }
 
 func (h *AuthHandler) Register(w http.ResponseWriter, r *http.Request) {
-	//
+	user := &User{
+		name:     r.FormValue("name"),
+		email:    r.FormValue("email"),
+		password: hashPassword(r.FormValue("password")),
+	}
+
+	_, err := h.app.DB.Exec(`
+		INSERT INTO users (name, email, password)
+		VALUES (?, ?, ?)
+	`, user.name, user.email, user.password)
+
+	if err != nil {
+		fmt.Fprint(w, `<p class="text-red-700 font-semibold">Fail</p>`)
+	} else {
+		fmt.Fprint(w, `<p class="text-green-700 font-semibold">Success</p>`)
+	}
 }
 
 func (h *AuthHandler) ShowLoginPage(w http.ResponseWriter, r *http.Request) {
@@ -57,4 +75,17 @@ func (h *AuthHandler) Login(w http.ResponseWriter, r *http.Request) {
 
 func (h *AuthHandler) Logout(w http.ResponseWriter, r *http.Request) {
 	//
+}
+
+func hashPassword(password string) string {
+	bytes, err := bcrypt.GenerateFromPassword([]byte(password), 14)
+	if err != nil {
+		panic(err)
+	}
+	return string(bytes)
+}
+
+func checkPasswordHash(password, hash string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+	return err == nil
 }
